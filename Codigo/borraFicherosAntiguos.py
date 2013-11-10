814957e585da67eb6f63651ad60ba065d0c1912f
root <root@raul-virtual-machine.(none)>
2013-11-10 21:14:31 +0100

    Final

--- Codigo/borraFicherosAntiguos.py
+++ Codigo/borraFicherosAntiguos.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
 
-# Practicas de Desarrollo de Aplicaciones para Internet (DAI)
-# Copyright (C) 2013 - Zerjillo (zerjioi@ugr.es)
-#    
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#   
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#   
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#Raúl Sánchez Fuentes Practica 2 IV
+#Copyright (C) 2013  Raúl
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 
         
-def borraFicherosAntiguos(directorio, prefijo, fechaAntiguedad):
+def borrar(directorio, prefijo, fechaAntiguedad):
     cadena = ""
     for filename in os.listdir(directorio):
         if filename.startswith(prefijo):
--- Codigo/ej1.py
+++ Codigo/ej1.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+#Raúl Sánchez Fuentes Practica 2 IV
+#Copyright (C) 2013  Raúl
 
-# Practicas de Desarrollo de Aplicaciones para Internet (DAI)
-# Copyright (C) 2013 - Zerjillo (zerjioi@ugr.es)
-#    
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#   
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#   
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
 
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 from formulario2 import *
